5ecb58d3c58e2db766a78fcfc25251ad69a35d9b
Basooma Anthony <anthbasooma@boku-it.boku.ac.at>
2024-04-24 16:42:26 +0200

    Model changes

--- R/prabs.R
+++ R/prabs.R
@@ -13,18 +13,14 @@
 #'
 #' @examples
 gen_background <- function(occurences, raster, coords=c('geometry'),
-                           class =list(present='P', absent='A'),
-                           prop, set.seed=1124){
+                           class =list(present='P', absent='A'), prop = 1, set.seed=1124){
 
-  if(missing(occurences)){
-    stop('Provide species presence data to generate the background records')
-  }
+  if(missing(occurences)) stop('Provide species presence data to generate the background records')
 
-  if(missing(raster)){stop('Provide environmental raster layer for point extraction')}
+  if(missing(raster))stop('Provide environmental raster layer for point extraction')
 
-  if(prop<0||prop>1) {
-    stop('Prop should be between 0 (not generated) and 1 (equal to length of presence)')
-  }
+  if(prop<0||prop>1) stop('Prop should be between 0 (not generated) and 1 (equal to length of presence')
+
 
   set.seed(set.seed)
 
@@ -40,7 +36,7 @@ gen_background <- function(occurences, raster, coords=c('geometry'),
 
   if(is(occurences, 'sf')){
 
-    bgpoints1 <- bgpoints1 %>%
+    bgpoints1 <- bgpoints1 |>
 
       sf::st_as_sf(coords =c('x', 'y'), crs=st_crs(4326))
 
