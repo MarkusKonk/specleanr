d9a69772eaa93ae8b633caed6913b065ab4d45d0
Anthony Basooma <anthony.basooma@boku.ac.at>
2024-07-09 01:00:59 +0200

    test dones

--- tests/testthat/test-basinranges.R
+++ tests/testthat/test-basinranges.R
@@ -2,15 +2,15 @@
 data("efidata")
 
 sal <- subset(efidata, scientificName=='Anguilla anguilla')
-#
-# testthat::test_that(desc = "Returns a datframe of records",
-#                     code = {
-#                       testthat::skip_if_offline()
-#                       testthat::skip_on_cran()
-#                       br <- basinranges(occurrences = sal, species = "Anguilla anguilla",
-#                                         lat = 'decimalLatitude', lon = 'decimalLongitude')
-#                       testthat::expect_s3_class(br, 'data.frame')
-#                     })
+
+testthat::test_that(desc = "Returns a datframe of records",
+                    code = {
+                      testthat::skip_if_offline()
+                      testthat::skip_on_cran()
+                      br <- basinranges(occurrences = sal, species = "Anguilla anguilla",
+                                        lat = 'decimalLatitude', lon = 'decimalLongitude')
+                      testthat::expect_s3_class(br, 'data.frame')
+                    })
 
 
 testthat::test_that("Latitude or Longitude missing in the data",
