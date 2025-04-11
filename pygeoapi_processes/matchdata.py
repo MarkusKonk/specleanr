326c496b556db1475628d981c9960784e09ea129
Merret <merret.buurman@igb-berlin.de>
2025-04-11 20:55:07 +0200

    ogc: all others: R error messages now get passed back to user in all processes.

--- pygeoapi_processes/matchdata.py
+++ pygeoapi_processes/matchdata.py
@@ -97,7 +97,7 @@ class DataMatchProcessor(BaseProcessor):
         ]
 
         ## Run the docker:
-        returncode, stdout, stderr = run_docker_container(
+        returncode, stdout, stderr, user_err_msg = run_docker_container(
             self.docker_executable,
             self.image_name,
             self.r_script,
@@ -106,7 +106,8 @@ class DataMatchProcessor(BaseProcessor):
         )
 
         if not returncode == 0:
-            err_msg = 'Running docker container failed.'
+            user_err_msg = "no message" if len(user_err_msg) == 0 else user_err_msg
+            err_msg = 'Running docker container failed: %s' % user_err_msg
             raise ProcessorExecuteError(user_msg = err_msg)
 
         # Return link to file:
@@ -210,14 +211,77 @@ def run_docker_container(
         stdout = result.stdout.decode()
         stderr = result.stderr.decode()
         LOGGER.debug('Finished running docker container')
-        return result.returncode, stdout, stderr
+        return result.returncode, stdout, stderr, "no error"
 
     except subprocess.CalledProcessError as e:
         returncode = e.returncode
         stdout = e.stdout.decode()
         stderr = e.stderr.decode()
         LOGGER.error('Failed running docker container (exit code %s)' % returncode)
-        for line in stderr.split('\n'):
-            if line:
-                LOGGER.error('Docker stderr: %s' % line)
-        return returncode, stdout, stderr
+        user_err_msg = get_error_message_from_docker_stderr(stderr)
+        return returncode, stdout, stderr, user_err_msg
+
+
+def get_error_message_from_docker_stderr(stderr, log_all_lines = True):
+    '''
+    We would like to return meaningful messages to users. For example, by
+    printing ALL stderr lines, we get the following:
+
+    ERROR - Docker stderr: Error in if (zz[which.max(zz)] < minpts) stop("All species do not have enough data after removing missing values and duplicates.") : 
+    ERROR - Docker stderr:   argument is of length zero
+    ERROR - Docker stderr: Calls: pred_extract
+    ERROR - Docker stderr: Execution halted
+
+    ERROR - Docker stderr: Error in pred_extract(data = speciesfiltered, raster = worldclim, lat = in_colname_lat,  : 
+    ERROR - Docker stderr:   All species do not have enough data after removing missing values and duplicates.
+    ERROR - Docker stderr: Execution halted
+
+    Now, how to capture the meaningful part of that, which we want to return
+    to the user? Here is a first attempt:
+    '''
+
+    user_err_msg = ""
+    error_on_previous_line = False
+    colon_on_previous_line = False
+    for line in stderr.split('\n'):
+
+        # Skip empty lines:
+        if not line:
+            continue
+
+        # Print all non-empty lines to log:
+        if log_all_lines:
+            LOGGER.error('Docker stderr: %s' % line)
+
+        # R error messages may start with the word "Error"
+        if line.startswith("Error"):
+            #LOGGER.debug('### Found explicit error line: %s' % line.strip())
+            user_err_msg += line.strip()
+            error_on_previous_line = True
+
+        # When R error messages are continued on another line, they may be
+        # indented by two spaces.
+        elif line.startswith("  ") and error_on_previous_line:
+            #LOGGER.debug('### Found indented line following an error: %s' % line.strip())
+            user_err_msg += " "+line.strip()
+            error_on_previous_line = True
+
+        # When R error messages end with a colon, they will be continued on
+        # the next line, independently of their indentation I guess!
+        elif colon_on_previous_line:
+            #LOGGER.debug('### Found line following a colon: %s' % line.strip())
+            user_err_msg += " "+line.strip()
+            error_on_previous_line = True
+
+        else:
+            #LOGGER.debug('### Do not pass back to user: %s' % line.strip())
+            error_on_previous_line = False
+
+        # Remember whether this line ended with a colon, indicating that the
+        # next line will continue with the error message:
+        colon_on_previous_line = False
+        if line.strip().endswith(":"):
+            #LOGGER.debug('### Found a colon, next line will still be error!')
+            colon_on_previous_line = True
+
+    return user_err_msg
