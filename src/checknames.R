1cb6bde8edea55e076de3bc0e5858d1447fb5f21
Merret <merret.buurman@igb-berlin.de>
2025-07-14 17:03:57 +0200

    src/checknames.R: Allowing as input either URL or species list.

--- src/checknames.R
+++ src/checknames.R
@@ -17,8 +17,8 @@ library(specleanr)
 
 args <- commandArgs(trailingOnly = TRUE)
 print(paste0('R Command line args: ', args))
-in_data_path              = args[1]
-in_colname_species        = args[2] # e.g. "species"
+in_data_path              = args[1] # a URL or a list of species
+in_colname_species        = args[2] # e.g. "species", or "NA" or "na"
 in_percent_correctness    = args[3] # e.g. "70"
 in_bool_merge             = args[4] # e.g. "true"
 in_bool_verbose           = args[5] #logical
@@ -29,32 +29,41 @@ out_result_path_names     = args[9]
 out_result_path_filtered  = args[10]
 
 
-# (1) Read data from CSV or from URL
-print(paste('Reading input data from CSV...'))
-mergealldfs <- data.table::fread(in_data_path)
+# Make string booleans boolean
+in_bool_merge = tolower(in_bool_merge) == 'true'
+in_bool_verbose = tolower(in_bool_verbose) == 'true'
 
 
-# (2) Make string booleans boolean
-in_bool_merge = tolower(in_bool_merge) == 'true' #confirms this
+# If there is NO column name, we assume that a list of species is given.
+# Otherwise, we assume an input CSV file is given.
+if (tolower(in_colname_species) == 'na') {
+  in_colname_species <- NULL
+  if (in_bool_verbose) message('DEBUG: No column name given, so we assume the user passed a list of species...')
+  if (in_bool_verbose) message('DEBUG: Splitting input arg species names...')
+  in_species_names <- in_data_path
+  in_species_names <- gsub(", ", ",", in_species_names, fixed = TRUE)
+  in_species_names <- gsub(" ,", ",", in_species_names, fixed = TRUE)
+  in_species_names <- strsplit(in_species_names, ",")[[1]]
+  if (in_bool_verbose) message('DEBUG: Found species names: ', paste(in_species_names, collapse=' + '))
+  mergealldfs <- in_species_names
+} else {
+  message('A column name was given, so we assume the user passed a file...')
+  message(paste0('Reading input data from CSV: ', in_data_path))
+  mergealldfs <- data.table::fread(in_data_path)
+}
 
 
-# (3) Remove spaces and split:
-print(paste('Splitting input arg species names...'))
-in_species_names = gsub(", ", ",", in_species_names, fixed = TRUE)
-in_species_names = gsub(" ,", ",", in_species_names, fixed = TRUE)
-in_species_names = strsplit(in_species_names, ",")[[1]]
-
-
-# (4) Run check_names:
-print('Running specleanr::check_names...')
-cleannames_df <- check_names(data = mergealldfs, 
-colsp = in_colname_species, 
-pct = as.numeric(in_pct), 
-merge = in_bool_merge,
-verbose = in_bool_verbose,
-sn = in_synonymn_checks,
-ecosystem = in_ecosystem_checks,
-rm_duplicates = in_rm_duplicates
+# Run check_names:
+if (in_bool_verbose) message('DEBUG: Running specleanr::check_names...')
+cleannames_df <- check_names(
+  data = mergealldfs,
+  colsp = in_colname_species,
+  pct = as.numeric(in_pct),
+  merge = in_bool_merge,
+  verbose = in_bool_verbose,
+  sn = in_synonymn_checks,
+  ecosystem = in_ecosystem_checks,
+  rm_duplicates = in_rm_duplicates
 )
 #cleannames_df <- check_names(data = mergealldfs, colsp = 'species', pct = 70, merge = TRUE)
 print('Running specleanr::check_names... DONE.')
