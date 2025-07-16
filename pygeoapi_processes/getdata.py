811946176ecf8f3023555828fd254fe03486526c
Merret <merret.buurman@igb-berlin.de>
2025-07-16 16:01:06 +0200

    pygeoapi: getdata: Allowing that a user sets no study area at all.

--- pygeoapi_processes/getdata.py
+++ pygeoapi_processes/getdata.py
@@ -133,12 +133,6 @@ class DataRetrievalProcessor(BaseProcessor):
         # Checks
         if in_data_path is None:
             raise ProcessorExecuteError('Provide the input data in either string or CSV format.')
-        if (study_area_bbox is None and
-            study_area_shp_url is None and
-            study_area_geojson_url is None and
-            study_area_geojson is None):
-            raise ProcessorExecuteError('Missing parameter "study_area". Please provide a URL to your input study area as zipped shapefile, as geojson (or just post geojson)...')
-
 
         #################################
         ### Input and output          ###
@@ -201,6 +195,9 @@ class DataRetrievalProcessor(BaseProcessor):
                 north = study_area_bbox["bbox"][2],
                 east  = study_area_bbox["bbox"][3]
             )
+
+        else:
+            in_extent = "null"
 
 
         ####################################
