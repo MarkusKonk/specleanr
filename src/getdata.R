aa8c7cd97b25e96955508067cbc97248c25ccbc4
Merret <merret.buurman@igb-berlin.de>
2025-07-21 18:48:14 +0200

    src/getdata: Cosmetics: comments and logging.

--- src/getdata.R
+++ src/getdata.R
@@ -16,6 +16,11 @@ print('Starting wrapper script: getdata.R.')
 library(specleanr)
 #print('DEBUG: Successfully imported library specleanr')
 
+##################################
+### Get command line arguments ###
+### as strings                 ###
+##################################
+
 args <- commandArgs(trailingOnly = TRUE)
 print(paste0('R Command line args: ', args))
 in_data_path        = args[1] #can be a dataset or species list. If species list then the colsp is empty
@@ -31,69 +36,88 @@ in_synonym_check    = args[10] #allow synoymns or not from FishBase
 in_warn_check       = args[11] #logical
 out_result_path     = args[12]
 
+##################################
+### Read input data from file: ###
+### study_area                 ###
+##################################
 
-#check if the bounding box is provided in a form of vector (xmin, ymin, xmax, ymax). otherwise load from shapefile
+# Check if the bounding box is NULL,
+# or provided in a form of named vector (xmin, ymin, xmax, ymax).
+# Otherwise load from shapefile
 
-if(tolower(in_extent)=='null'){ #
+# study area is NULL:
+if(tolower(in_extent)=='null'){
 
   study_area = NULL
   
+# study area is a file:
 }else if(startsWith(in_extent, 'http') | file.exists(in_extent)){
 
-  # If the URL points to a zipped shape, download and unzip before we can read it:
+  # If the URL points to a zipped shapefile, download and unzip before we can read it:
   if (startsWith(in_extent, 'http') & endsWith(in_extent, 'zip')) {
+    message("DEBUG: Downloading zipped shapefile: ", in_bbox_path)
     temp_zip <- tempfile(fileext = ".zip")
     download.file(in_extent, temp_zip, mode = "wb")
     unzip(temp_zip, exdir = tempdir())
     in_extent <- list.files(tempdir(), pattern = "\\.shp$", full.names = TRUE)
+
+  # If the URL points to a non-zipped shapefile, throw error:
   } else if (startsWith(in_extent, 'http') & endsWith(in_extent, 'shp')) {
     stop('If you specify a remote shapefile as input, please zip it...')
   }
-  # (1) Read data from shapefile
+
+# Read data from shapefile
 # TODO Test, can st_read also read GeoJSON? It should?
-print(paste('Reading input data from shapefile or GeoJSON:',in_extent))
-#print(paste('DEBUG: Content of directory ', dirname(in_data_path), ':', paste(list.files(dirname(in_data_path)), collapse=", ")))
+message('DEBUG: Reading input data from shapefile or GeoJSON:',in_extent)
+#message('DEBUG: Content of directory ', dirname(in_data_path), ':', paste(list.files(dirname(in_data_path)), collapse=", "))
 study_area <- sf::st_read(in_extent, quiet=TRUE)
 
-
+# study area is a bounding box:
   }else{
     #study_area <- sf::st_read(system.file('extdata/danube/basinfinal.shp', package = 'specleanr'), quiet=TRUE)
   x = in_extent
-  message("Splitting the string to form a vector of bounding box... (Must be provided as named list, in this format: 'xmin=8.15250, ymin=42.08333, xmax=29.73583, ymax=50.24500')")
+  message("DEBUG: Splitting the string to form a vector of bounding box... (Must be provided as named list, in this format: 'xmin=8.15250, ymin=42.08333, xmax=29.73583, ymax=50.24500')")
   study_area = eval(parse(text = paste0("list(", x, ")")))
-  
 }
 
+##################################
+### Read input data from file: ###
+### speciesdata                ###
+##################################
 
+# if species data is a file, read it:
 if(startsWith(in_data_path, 'http') | file.exists(in_data_path)) {
-
 speciesdata <- data.table::fread(in_data_path)
 
+# if species are given as a comma-separated list, remove spaces and split:
 }else{
-  # (2) Remove spaces and split:
-print(paste('Splitting input arg species names...'))
+message('DEBUG: Splitting input arg species names...')
 in_data_path = gsub(", ", ",", in_data_path, fixed = TRUE)
 in_data_path = gsub(" ,", ",", in_data_path, fixed = TRUE)
 speciesdata = strsplit(in_data_path, ",")[[1]]
 }
 
+################################
+### Convert string arguments ###
+### required to R data types ###
+################################
 
-#database list
+# List of database names
 in_database = gsub(", ", ",", in_database, fixed = TRUE)
 in_database = gsub(" ,", ",", in_database, fixed = TRUE)
 in_database = strsplit(in_database, ",")[[1]]
 
-
-
-
-# (3) Make limits numeric
+# Make limits numeric
 in_gbif_lim = as.numeric(in_gbif_lim)
 in_inat_lim = as.numeric(in_inat_lim)
 in_vert_lim = as.numeric(in_vert_lim)
 
 
-# (3) Run getdata:
-print('Running specleanr::getdata...')
+##############################
+### Run specleanr function ###
+##############################
+
+message('DEBUG: Running specleanr::getdata...')
 df_online <- getdata(
   data     = speciesdata, 
   colsp    = in_species_column,
@@ -107,9 +131,9 @@ df_online <- getdata(
   warn = in_warn_check,
   pct =  in_percent_correct
   )
-print('Running specleanr::getdata... DONE.')
+message('DEBUG: Running specleanr::getdata... DONE.')
 
 
-# (4) Write the result to csv file:
-print(paste0('Write result to csv file: ', out_result_path))
+# Write the result to csv file:
+message('DEBUG: Write result to csv file: ', out_result_path)
 data.table::fwrite(df_online , file = out_result_path)
