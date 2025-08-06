1f6f74330d8f85f2e23ba0f642603781238e4339
Merret <merret.buurman@igb-berlin.de>
2025-08-06 11:11:55 +0200

    pygeoapi: utils: Cosmetics, just moved function down.

--- pygeoapi_processes/utils.py
+++ pygeoapi_processes/utils.py
@@ -6,66 +6,6 @@ import os
 LOGGER = logging.getLogger(__name__)
 
 
-def call_r_script(LOGGER, r_file_name, path_rscripts, r_args):
-
-    # Call R script:
-    r_file = path_rscripts.rstrip('/')+os.sep+r_file_name
-    cmd = ["/usr/bin/Rscript", "--vanilla", r_file] + r_args
-    LOGGER.debug('Running command %s ... (Output will be shown once finished)' % r_file_name)
-    LOGGER.info(cmd)
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdoutdata, stderrdata = p.communicate()
-    LOGGER.debug("Done running command! Exit code from bash: %s" % p.returncode)
-
-    # Retrieve stdout and stderr
-    stdouttext = stdoutdata.decode()
-    stderrtext = stderrdata.decode()
-
-    # Remove empty lines:
-    stderrtext_new = ''
-    for line in stderrtext.split('\n'):
-        if len(line.strip())==0:
-            LOGGER.debug('Empty line!')
-        else:
-            LOGGER.debug('Non-empty line: %s' % line)
-            stderrtext_new += line+'\n'
-
-    # Remove empty lines:
-    stdouttext_new = ''
-    for line in stdouttext.split('\n'):
-        if len(line.strip())==0:
-            LOGGER.debug('Empty line!')
-        else:
-            LOGGER.debug('Non-empty line: %s' % line)
-            stdouttext_new += line+'\n'
-
-    stderrtext = stderrtext_new
-    stdouttext = stdouttext_new
-
-    # Format stderr/stdout for logging:
-    if len(stderrdata) > 0:
-        err_and_out = 'R stdout and stderr:\n___PROCESS OUTPUT {name} ___\n___stdout___\n{stdout}\n___stderr___\n{stderr}\n___END PROCESS OUTPUT {name} ___\n______________________'.format(
-            name=r_file_name, stdout=stdouttext, stderr=stderrtext)
-        LOGGER.error(err_and_out)
-    else:
-        err_and_out = 'R stdour:\n___PROCESS OUTPUT {name} ___\n___stdout___\n{stdout}\n___stderr___\n___(Nothing written to stderr)___\n___END PROCESS OUTPUT {name} ___\n______________________'.format(
-            name=r_file_name, stdout=stdouttext)
-        LOGGER.info(err_and_out)
-
-    # Extract error message from R output, if applicable:
-    err_msg = None
-    if not p.returncode == 0:
-        err_msg = 'R script "%s" failed.' % r_file_name
-        for line in stderrtext.split('\n'):
-            line = line.strip().lower()
-            if line.startswith('error') or line.startswith('fatal') or 'error' in line:
-                LOGGER.error('FOUND R ERROR LINE: %s' % line)
-                err_msg += ' '+line.strip()
-                LOGGER.error('ENTIRE R ERROR MSG NOW: %s' % err_msg)
-
-    return p.returncode, stdouttext, stderrtext, err_msg
-
-
 
 
 def run_docker_container(
@@ -354,3 +294,67 @@ def store_geojson(geojson, input_dir, ending=None):
         json.dump(geojson, myfile)
 
     return input_file_path
+
+
+
+def call_r_script(LOGGER, r_file_name, path_rscripts, r_args):
+    # Not needed anymore, as we call docker container now, not rscripts.
+    # But keeping it here, maybe needed at some point...
+
+    # Call R script:
+    r_file = path_rscripts.rstrip('/')+os.sep+r_file_name
+    cmd = ["/usr/bin/Rscript", "--vanilla", r_file] + r_args
+    LOGGER.debug('Running command %s ... (Output will be shown once finished)' % r_file_name)
+    LOGGER.info(cmd)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdoutdata, stderrdata = p.communicate()
+    LOGGER.debug("Done running command! Exit code from bash: %s" % p.returncode)
+
+    # Retrieve stdout and stderr
+    stdouttext = stdoutdata.decode()
+    stderrtext = stderrdata.decode()
+
+    # Remove empty lines:
+    stderrtext_new = ''
+    for line in stderrtext.split('\n'):
+        if len(line.strip())==0:
+            LOGGER.debug('Empty line!')
+        else:
+            LOGGER.debug('Non-empty line: %s' % line)
+            stderrtext_new += line+'\n'
+
+    # Remove empty lines:
+    stdouttext_new = ''
+    for line in stdouttext.split('\n'):
+        if len(line.strip())==0:
+            LOGGER.debug('Empty line!')
+        else:
+            LOGGER.debug('Non-empty line: %s' % line)
+            stdouttext_new += line+'\n'
+
+    stderrtext = stderrtext_new
+    stdouttext = stdouttext_new
+
+    # Format stderr/stdout for logging:
+    if len(stderrdata) > 0:
+        err_and_out = 'R stdout and stderr:\n___PROCESS OUTPUT {name} ___\n___stdout___\n{stdout}\n___stderr___\n{stderr}\n___END PROCESS OUTPUT {name} ___\n______________________'.format(
+            name=r_file_name, stdout=stdouttext, stderr=stderrtext)
+        LOGGER.error(err_and_out)
+    else:
+        err_and_out = 'R stdour:\n___PROCESS OUTPUT {name} ___\n___stdout___\n{stdout}\n___stderr___\n___(Nothing written to stderr)___\n___END PROCESS OUTPUT {name} ___\n______________________'.format(
+            name=r_file_name, stdout=stdouttext)
+        LOGGER.info(err_and_out)
+
+    # Extract error message from R output, if applicable:
+    err_msg = None
+    if not p.returncode == 0:
+        err_msg = 'R script "%s" failed.' % r_file_name
+        for line in stderrtext.split('\n'):
+            line = line.strip().lower()
+            if line.startswith('error') or line.startswith('fatal') or 'error' in line:
+                LOGGER.error('FOUND R ERROR LINE: %s' % line)
+                err_msg += ' '+line.strip()
+                LOGGER.error('ENTIRE R ERROR MSG NOW: %s' % err_msg)
+
+    return p.returncode, stdouttext, stderrtext, err_msg
+
